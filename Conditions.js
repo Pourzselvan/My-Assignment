9dbe2a12099ec0a715277c24ef39790c99f0ebec
Pourzselvan <p.m.pourzselvan@gmail.com>
2026-05-18 20:58:14 +0900

    Add files via upload

---
+++ Conditions.js
@@ -0,0 +1,18 @@
+let a = 24 , b=77
+
+if((a%2) == 0)
+{
+    console.log ("Even")
+}
+else
+{
+    console.log ("Odd")
+}
+if((b%2) == 0)
+{
+    console.log ("Even")
+}
+else
+{
+    console.log ("Odd")
+}
