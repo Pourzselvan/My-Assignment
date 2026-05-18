27aba1ff97dbe8963e85d290bf28143d4631f696
Pourzselvan <p.m.pourzselvan@gmail.com>
2026-05-18 20:57:38 +0900

    Add files via upload
    Day-2

---
+++ varconslet.js
@@ -0,0 +1,25 @@
+const browserVersion ="Chrome"
+
+console.log(browserVersion)
+
+function getBrowserVersion1()
+{
+  
+    {
+    var browserVersion = "Firefox"
+    }
+console.log(browserVersion)
+}
+
+getBrowserVersion1()
+
+function getBrowserVersion2()
+{
+ 
+    {
+    let browserVersion= "edge"
+    }
+console.log(browserVersion)
+}
+
+getBrowserVersion2()
