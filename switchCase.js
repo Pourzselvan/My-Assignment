c0bb7f32f0fd4e80f1e6c1953052272dc66c4c1f
Pourzselvan <p.m.pourzselvan@gmail.com>
2026-05-19 21:36:13 +0900

    Add files via upload

---
+++ switchCase.js
@@ -0,0 +1,30 @@
+let Maths = 65 ,science = 78, English = 66
+
+Total = Maths + science + English
+
+
+    switch (true) {
+        case Total >=250:
+
+        console.log("Grade A")
+            
+            break;
+
+        case Total >=200:
+
+        console.log("Grade B")
+            
+            break;
+
+        case Total >=150:
+
+        console.log("Grade C")
+            
+            break;
+
+    
+        default:
+            
+        console.log("Fail")
+            break;
+    }
